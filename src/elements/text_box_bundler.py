f674c57b7f14d0be2840dbdc9c47914f05969035
Kirschzelle <nikolassc@edu.aau.at>
2025-11-16 11:44:43 +0100

    forgot to add poebel person to textbox so it actually displays him

--- src/elements/text_box_bundler.py
+++ src/elements/text_box_bundler.py
@@ -10,11 +10,6 @@ class TextBoxBundler():
         self.tb_world = tb.TextBox(c.NATIVE_W*(412/480),c.NATIVE_H*(111/270),c.NATIVE_W*(150/480),c.NATIVE_H*(115/270),-10)
         self.tb_poebel = tb.TextBox(c.NATIVE_W*(350/480),c.NATIVE_H*(257/270),c.NATIVE_W*(270/480),c.NATIVE_H*(80/270),-7)
         self.tb_player = tb.TextBox(c.NATIVE_W*(175/480),c.NATIVE_H*(35/270),c.NATIVE_W*(330/480),c.NATIVE_H*(25/270),0, font_name = "My Soul", prevent_overflow = False)
-        #self.tb_council.set_text_instant("""[s:8]Hello! This is [c:RED]red text[c:BLACK].
-#Now switching to [f:Courier New]monospace font[f:My Soul].
-#Here's [c:BLUE][s:12]BIG BLUE TEXT[s:14][c:BLACK]!
-#You can also use [c:255,0,255]RGB colors[c:BLACK].
-#[c:GREEN]Green text with [s:18]different sizes[s:14]![c:BLACK]""")
 
     def draw(self, scale):
         self.tb_council.draw(scale)
@@ -31,10 +26,12 @@ class TextBoxBundler():
         self.tb_player.update(delta_time)
 
     def add_message(self, sender, message):
-        print(f"[DEBUG] Appendet message:\n-[Sender]{sender}\n-[Message]{message}")
+        print(f"[DEBUG] Appended message:\n-[Sender]{sender}\n-[Message]{message}")
         if sender == c.PLAYER:
             self.tb_council.append_text(f"\n[c:AFRICAN_VIOLET][f:My Soul]{sender}: {message}")
         elif sender == c.KING:
             self.tb_king.append_text(f"\n[c:GOLD][f:My Soul]{sender}: {message}")
         elif sender == c.GREEDY_BASTARD:
             self.tb_council.append_text(f"\n[c:OCEAN_BOAT_BLUE][f:Arial]{sender}: {message}")
+        elif sender == c.POEBEL:
+            self.tb_poebel.append_text(f"\n[c:BITTER_LEMON][f:Monoton]{sender}: {message}")
